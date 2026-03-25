d303064bb494fd531f3fc6f1fc3b85676e4a9611
322mason-dev <322mason@gmail.com>
2026-03-25 14:05:56 -0400

    Create hours.js

---
+++ html/portfolio/MapleGlenPizza/data/hours.js
@@ -0,0 +1,29 @@
+[
+    {"day": "Sunday",
+    "open": "11:00AM",
+    "close": "9:00PM"},
+
+    {"day": "Monday",
+    "open": "10:00AM",
+    "close": "9:00PM"},
+
+    {"day": "Tuesday",
+    "open": "10:00AM",
+    "close": "9:00PM"},
+
+    {"day": "Wednesday",
+    "open": "10:00AM",
+    "close": "9:00PM"},
+
+    {"day": "Thursday",
+    "open": "10:00AM",
+    "close": "9:00PM"},
+
+    {"day": "Friday",
+    "open": "10:00AM",
+    "close": "10:00PM"},
+
+    {"day": "Saturday",
+    "open": "10:00AM",
+    "close": "10:00PM"}
+]
