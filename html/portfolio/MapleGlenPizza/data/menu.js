9c93a6067c3c898d73f3bbb822fdbe55a2b6e014
322mason-dev <322mason@gmail.com>
2026-03-25 14:06:17 -0400

    Create menu.js

---
+++ html/portfolio/MapleGlenPizza/data/menu.js
@@ -0,0 +1,98 @@
+{
+    "sections": [
+        {
+            "id": "pizza",
+            "name": "Pizza",
+            "items": [
+                {
+                  "name": "Plain Cheese",
+                  "desc": "Classic tomato sauce and mozzarella.",
+                  "price": 12.99
+                },
+                {
+                  "name": "Pepperoni",
+                  "desc": "Crispy pepperoni with extra mozzarella.",
+                  "price": 14.99
+                },
+                {
+                  "name": "Margherita",
+                  "desc": "Fresh mozzarella, basil, olive oil.",
+                  "price": 14.49
+                },
+                {
+                  "name": "Veggie",
+                  "desc": "Peppers, onions, mushrooms, olives.",
+                  "price": 15.49
+                },
+                {
+                  "name": "BBQ Chicken",
+                  "desc": "BBQ sauce, grilled chicken, red onion.",
+                  "price": 16.49
+                }
+            ]
+        },
+        {
+            "id": "hoagies",
+            "name": "Hoagies",
+            "items": [
+                {
+                  "name": "Italian Hoagie",
+                  "desc": "Ham, salami, provolone, lettuce, tomato.",
+                  "price": 10.99
+                },
+                {
+                  "name": "Cheesesteak",
+                  "desc": "Steak, grilled onions, American cheese.",
+                  "price": 11.99
+                },
+                {
+                  "name": "Chicken Cheesesteak",
+                  "desc": "Grilled chicken, American cheese.",
+                  "price": 11.99
+                },
+                {
+                  "name": "Meatball Parm",
+                  "desc": "House meatballs, marinara, mozzarella.",
+                  "price": 11.49
+                }
+            ]
+        },
+        {
+            "id": "sides",
+            "name": "Sides",
+            "items": [
+                {
+                  "name": "Garlic Knots",
+                  "desc": "Six knots served with marinara.",
+                  "price": 6.49
+                },
+                {
+                  "name": "Mozzarella Sticks",
+                  "desc": "Fried mozzarella with marinara.",
+                  "price": 7.49
+                },
+                {
+                  "name": "French Fries",
+                  "desc": "Golden and crispy.",
+                  "price": 4.99
+                }
+            ]
+        },
+        {
+            "id": "drinks",
+            "name": "Drinks",
+            "items": [
+                {
+                "name": "2L Soda",
+                "desc": "Coke, Diet Coke, Sprite.",
+                "price": 3.49
+                },
+                {
+                "name": "20oz Bottle",
+                "desc": "Coke, Diet Coke, Sprite.",
+                "price": 2.49
+                }
+            ]
+        }
+    ]
+}
